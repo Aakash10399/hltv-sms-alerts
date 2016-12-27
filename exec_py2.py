1f19bfd8caded76a3bea6f87b2efc609d6811129
Aakash Pahwa <aakash.pahwa10399cric@gmail.com>
2016-12-28 01:34:07 +0530

    UPDATE_4

--- exec_py2.py
+++ exec_py2.py
@@ -7,59 +7,67 @@ import os
 from stat import *
 import re
 import time
-
 def checkForUpdates():
-    msg= requests.get("http://www.hltv.org/").text
-    msg= msg[msg.find("newsRight"):]
-    msg= msg[msg.find("<b>")+3:msg.find("</b>")]    
-    flag=0
-    readMe = open('hltv.txt','r').read()    
-    if readMe!=msg:
-        flag=1
-        saveFile = open('hltv.txt','w')
-        saveFile.write(msg)
-        saveFile.close()
-    return flag    
+    try:
+        msg= requests.get("http://www.hltv.org/").text
+        msg= msg[msg.find("newsRight"):]
+        msg= msg[msg.find("<b>")+3:msg.find("</b>")]    
+        flag=0
+        readMe = open('hltv.txt','r').read()    
+        if readMe!=msg:
+            flag=1
+            saveFile = open('hltv.txt','w')
+            saveFile.write(msg)
+            saveFile.close()
+        return flag 
+    except:
+        return 0
 def hltvInfoGet(num,username,passwd):
-    msg= requests.get("http://www.hltv.org/").text
-    msg= msg[msg.find("newsRight"):]
-    msg= msg[msg.find("<b>")+3:msg.find("</b>")]
-    msg1= requests.get("http://www.hltv.org/").text
-    msg1= msg1[msg1.find("newsRight"):]
-    msg1= msg1[msg1.find("</b>")+4:]
-    msg1= msg1[msg1.find("<b>")+3:msg1.find("</b>")]
-    msg = "CSGO NEWS: "+msg+" , "+msg1+". Check www.hltv.org for more info."
-    msg= msg.replace("&amp;","and")
-    smsSend(msg,num,username,passwd)
-def smsSend(message,number,username,passwd):    
-    message = "+".join(message.split(' '))
-    url ='http://site24.way2sms.com/Login1.action?'
-    data = 'username='+username+'&password='+passwd+'&Submit=Sign+in'
-    cj= cookielib.CookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    opener.addheaders=[('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120')]
     try:
-        usock =opener.open(url, data)
-    except IOError:
-        print "error"
-    jession_id =str(cj).split('~')[1].split(' ')[0]
-    send_sms_url = 'http://site24.way2sms.com/smstoss.action?'
-    send_sms_data = 'ssaction=ss&Token='+jession_id+'&mobile='+number+'&message='+message+'&msgLen=136'
-    opener.addheaders=[('Referer', 'http://site25.way2sms.com/sendSMS?Token='+jession_id)]
-    try:
-        sms_sent_page = opener.open(send_sms_url,send_sms_data)
-    except IOError:
-        print "error"       
-    print "success" 
+        msg= requests.get("http://www.hltv.org/").text
+        msg= msg[msg.find("newsRight"):]
+        msg= msg[msg.find("<b>")+3:msg.find("</b>")]
+        msg1= requests.get("http://www.hltv.org/").text
+        msg1= msg1[msg1.find("newsRight"):]
+        msg1= msg1[msg1.find("</b>")+4:]
+        msg1= msg1[msg1.find("<b>")+3:msg1.find("</b>")]
+        msg = "CSGO NEWS: "+msg+" , "+msg1+". Check www.hltv.org for more info."
+        msg= msg.replace("&amp;","and")
+        smsSend(msg,num,username,passwd)
+    except:
+        cronJob()
+def smsSend(message,number,username,passwd): 
+    try:   
+        message = "+".join(message.split(' '))
+        url ='http://site24.way2sms.com/Login1.action?'
+        data = 'username='+username+'&password='+passwd+'&Submit=Sign+in'
+        cj= cookielib.CookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+        opener.addheaders=[('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120')]
+        try:
+            usock =opener.open(url, data)
+        except IOError:
+            print "error"
+        jession_id =str(cj).split('~')[1].split(' ')[0]
+        send_sms_url = 'http://site24.way2sms.com/smstoss.action?'
+        send_sms_data = 'ssaction=ss&Token='+jession_id+'&mobile='+number+'&message='+message+'&msgLen=136'
+        opener.addheaders=[('Referer', 'http://site25.way2sms.com/sendSMS?Token='+jession_id)]
+        try:
+            sms_sent_page = opener.open(send_sms_url,send_sms_data)
+        except IOError:
+            print "error"       
+        print "success" 
+    except:
+        cronJob()       
 def cronJob():
     while 0==0:
         rt = checkForUpdates()
         if rt==1:
-            hltvInfoGet("number_to_send","way2smsuid","way2smspass")
+            hltvInfoGet("number_to_send","way2smsuid","way2smspass") #Aakash Pahwa
             print "********************************"    
         else:
             print "Old News"
             print "********************************"   
-        refresh_rate = 1800 #in seconds
+        refresh_rate = 10 #in seconds
         time.sleep(refresh_rate)  
 cronJob()
