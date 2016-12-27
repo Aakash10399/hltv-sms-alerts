4d02b15b1c8e4f3e43e941235320ff46ecc793a1
Aakash Pahwa <aakash.pahwa10399cric@gmail.com>
2016-12-28 00:32:05 +0530

    UPDATE_3

--- exec_py2.py
+++ exec_py2.py
@@ -7,30 +7,31 @@ import os
 from stat import *
 import re
 import time
-def hltvInfoGet(num):
+
+def checkForUpdates():
     msg= requests.get("http://www.hltv.org/").text
     msg= msg[msg.find("newsRight"):]
-    msg= msg[msg.find("<b>")+3:msg.find("</b>")]
+    msg= msg[msg.find("<b>")+3:msg.find("</b>")]    
     flag=0
-    readMe = open('hltv.txt','r').read()
+    readMe = open('hltv.txt','r').read()    
     if readMe!=msg:
         flag=1
         saveFile = open('hltv.txt','w')
         saveFile.write(msg)
         saveFile.close()
+    return flag    
+def hltvInfoGet(num,username,passwd):
+    msg= requests.get("http://www.hltv.org/").text
+    msg= msg[msg.find("newsRight"):]
+    msg= msg[msg.find("<b>")+3:msg.find("</b>")]
     msg1= requests.get("http://www.hltv.org/").text
     msg1= msg1[msg1.find("newsRight"):]
     msg1= msg1[msg1.find("</b>")+4:]
     msg1= msg1[msg1.find("<b>")+3:msg1.find("</b>")]
     msg = "CSGO NEWS: "+msg+" , "+msg1+". Check www.hltv.org for more info."
     msg= msg.replace("&amp;","and")
-    if flag==1:
-        smsSend(msg,num)
-    else:
-        print "Old News"
-def smsSend(message,number):    # Saiwal Krishna's code
-    username = "way2smsuid"
-    passwd = "way2smspass"
+    smsSend(msg,num,username,passwd)
+def smsSend(message,number,username,passwd):    
     message = "+".join(message.split(' '))
     url ='http://site24.way2sms.com/Login1.action?'
     data = 'username='+username+'&password='+passwd+'&Submit=Sign+in'
@@ -52,7 +53,13 @@ def smsSend(message,number):    # Saiwal Krishna's code
     print "success" 
 def cronJob():
     while 0==0:
-        hltvInfoGet("number_to_send")
-        refresh_rate = 5 #in seconds
-        time.sleep(refresh_rate)
+        rt = checkForUpdates()
+        if rt==1:
+            hltvInfoGet("number_to_send","way2smsuid","way2smspass")
+            print "********************************"    
+        else:
+            print "Old News"
+            print "********************************"   
+        refresh_rate = 1800 #in seconds
+        time.sleep(refresh_rate)  
 cronJob()
