4ce7ce955a3dc9ea2a5e44ce304203c677319c22
Aakash Pahwa <aakash.pahwa10399cric@gmail.com>
2016-12-31 13:52:48 +0530

    UPDATE_5

--- exec_py3.py
+++ exec_py3.py
@@ -7,9 +7,12 @@ import os
 from stat import *
 import re
 import time
+import unirest
+msg_base= requests.get("http://www.hltv.org/").text
 def checkForUpdates():
+    global msg_base
     try:
-        msg= requests.get("http://www.hltv.org/").text
+        msg= msg_base
         msg= msg[msg.find("newsRight"):]
         msg= msg[msg.find("<b>")+3:msg.find("</b>")]    
         flag=0
@@ -23,15 +26,21 @@ def checkForUpdates():
     except:
         return 0
 def hltvInfoGet(num,username,passwd):
+    global msg_base
     try:
-        msg= requests.get("http://www.hltv.org/").text
+        msg= msg_base
         msg= msg[msg.find("newsRight"):]
         msg= msg[msg.find("<b>")+3:msg.find("</b>")]
-        msg1= requests.get("http://www.hltv.org/").text
+        msg1= msg_base
         msg1= msg1[msg1.find("newsRight"):]
         msg1= msg1[msg1.find("</b>")+4:]
         msg1= msg1[msg1.find("<b>")+3:msg1.find("</b>")]
-        msg = "CSGO NEWS: "+msg+" , "+msg1+". Check www.hltv.org for more info."
+        msg2= msg_base
+        msg2= msg2[msg2.find("newsRight"):]
+        msg2= msg2[msg2.find("</b>")+4:]
+        msg2= msg2[msg2.find("</b>")+4:]
+        msg2= msg2[msg2.find("<b>")+3:msg2.find("</b>")]
+        msg = "CS NEWS: "+msg+", "+msg1+", "+msg2
         msg= msg.replace("&amp;","and")
         smsSend(msg,num,username,passwd)
     except:
@@ -55,15 +64,17 @@ def smsSend(message,number,username,passwd):
         try:
             sms_sent_page = opener.open(send_sms_url,send_sms_data)
         except IOError:
-            print("error")       
-        print("success") 
+            print("error")      
+        print("success")
     except:
         cronJob()       
 def cronJob():
+    global msg_base
     while 0==0:
+        msg_base= requests.get("http://www.hltv.org/").text
         rt = checkForUpdates()
         if rt==1:
-            hltvInfoGet("number_to_send","way2smsuid","way2smspass") #Aakash Pahwa
+            hltvInfoGet("number_to_send","way2sms_uid","way2sms_pass") #Aakash Pahwa     
             print("********************************")    
         else:
             print("Old News")
